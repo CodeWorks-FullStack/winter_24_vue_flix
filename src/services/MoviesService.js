7783a2ee295fcf717750c9f2fc0b692b908adac0
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-28 10:26:31 -0700

    storing response data in appstate

--- src/services/MoviesService.js
+++ src/services/MoviesService.js
@@ -1,3 +1,5 @@
+import { AppState } from "../AppState.js"
+import { Movie } from "../models/Movie.js"
 import { logger } from "../utils/Logger.js"
 import { movieApi } from "./AxiosService.js"
 
@@ -5,6 +7,8 @@ class MoviesService {
   async getMovies() {
     const response = await movieApi.get('discover/movie')
     logger.log('📡 GOT MOVIES', response.data)
+    const newMovies = response.data.results.map(moviePOJO => new Movie(moviePOJO))
+    AppState.movies = newMovies
   }
 }
 
